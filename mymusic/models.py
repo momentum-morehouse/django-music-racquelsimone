8ec306d725a8e2de04877f87fc599a3faaa01e3d
Racquel WS <racquelwsf@gmail.com>
2020-07-15 21:06:58 +0000

    Images! Still need to work on horizontal alignment. Le sigh,

--- mymusic/models.py
+++ mymusic/models.py
@@ -5,7 +5,8 @@ from django.db import models
 class Album(models.Model):
   artist_name = models.CharField(max_length=255, null=True, blank=True)
   album_title = models.CharField(max_length=255, null=True, blank=True)
-  date_released = models.DateField(null = True, blank=True)
-
-  def _str_(self):
+  date_released = models.DateField(null= True, blank=True)
+  image_url = models.TextField(null= True, blank=True)
+  
+  def __str__(self):
     return f"{self.album_title}"
